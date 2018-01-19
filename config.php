7f96dbf1647cfaf69134f24b5dd2ed062711a34a
Abdulsamet SAYLIK <rexox@hotmail.de>
2018-01-19 23:05:00 +0300

    Update config.php
    api.php migration

--- config.php
+++ config.php
@@ -1,10 +1,6 @@
 <?php
-if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
-  throw new \Exception('please run "composer require google/apiclient:~2.0" in "' . __DIR__ .'"');
-}
-
-require_once __DIR__ . '/vendor/autoload.php';
 require_once 'db.php';
+require_once 'api.php';
 session_start();
 
 // Fill CLIENT ID, CLIENT SECRET ID, REDIRECT URI from Google Developer Console
@@ -12,15 +8,10 @@ session_start();
  $client_secret = 'CLIENT_SECRET';
  $redirect_uri = 'REDIRECT_URI';
  $API_KEY='API_KEY';
- 
-//Create Client Request to access Google API
-$client = new Google_Client();
-$client->setApplicationName("YoutubeSubs");
-$client->setClientId($client_id);
-$client->setClientSecret($client_secret);
-$client->setScopes('https://www.googleapis.com/auth/youtube');
-$client->setRedirectUri($redirect_uri);
-
-// Define an object that will be used to make all API requests.
-$youtube = new Google_Service_YouTube($client);
-
+ $api = new YouTubeAPI();
+ $api->client_id=$client_id;
+ $api->client_secret=$client_secret;
+ $api->redirect_uri=$redirect_uri;
+ $api->API_KEY=$API_KEY;
+ $client = $api->create();
+ $youtube=$api->youtube($client);
